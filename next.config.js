52a4a2d7d999b5f7a395d84ce21a77d98d11d97d
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-08 10:01:09 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const path = require('path');
-
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-};
+module.exports = nextConfig
