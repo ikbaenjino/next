3483241a41ddf4b74e362e120b66bc5c722f4b13
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-08 09:30:40 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 // tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-      extend: {
-        // Anda bisa menambahkan kustomisasi tema di sini
-      },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF2D95",
+        accent:  "#000000"
+      }
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+}
