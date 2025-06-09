e46ad4697c916e462161051a8a4b3702105419b5
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-09 16:24:28 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import LanguageToggle from "../components/LanguageToggle";
+
 
 export const metadata = {
   title: 'Koding Next Clone',
@@ -11,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <body className="font-sans bg-white text-gray-800">
+        <LanguageToggle />
         <Navbar />
         <main>{children}</main>
         <Footer />
