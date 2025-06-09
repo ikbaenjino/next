11e0baafe1bbbdd1f4111bb3738345fc1a7d217b
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-09 16:09:47 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Programs from "../components/Programs";
 import Methods from "../components/Methods";
 import Stats from "../components/Stats";
 import Testimonials from "../components/Testimonials";
+import Locations from "../components/Locations"; // ✅ Tambahkan baris ini
 import Footer from "../components/Footer";
 
 export default function HomePage() {
@@ -13,7 +14,8 @@ export default function HomePage() {
       <Methods />
       <Stats />
       <Testimonials />
-      <Footer /> {/* ✅ Tambahkan Footer di akhir */}
+      <Locations /> {/* ✅ Tambahkan baris ini */}
+      <Footer />
     </main>
   );
 }
