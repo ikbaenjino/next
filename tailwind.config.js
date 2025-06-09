a2b2b90a49dbad54a643b880ad429ba71ce85760
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-09 21:16:27 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
+    },
+  },
+};
   theme: {
     extend: {
       colors: {
