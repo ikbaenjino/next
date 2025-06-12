374ca8ad6fa4135cf43d28a8cc0806f9837f7018
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-12 23:56:52 +0700

    tambahan

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "../styles/tailwind.css"
+import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import LanguageToggle from '../components/LanguageToggle';
