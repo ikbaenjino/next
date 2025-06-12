0194f909f388c31d0308ef362dcc5815ca1f4686
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-12 20:09:04 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,8 +20,7 @@ export default function HomePage() {
       <Testimonials />
       <Events />
       <FAQ />
-      <Locations />
-      <Footer />
+      <Locations />      
     </main>
   );
 }
