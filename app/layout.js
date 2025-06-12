871886f5d4889901c540b8c1f1bf996298f86590
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-13 00:17:20 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
     url: "https://next-zttx.vercel.app", 
     images: [
       {
-        url: "/images/hero.png",
+        url: "/images/hero-banner.jpg",
         width: 1200,
         height: 630,
         alt: "Koding Next - Sekolah Coding",
