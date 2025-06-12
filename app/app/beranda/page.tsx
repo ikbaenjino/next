1e90124ffaafa9e73f5c3ec9ab597edfc4c73012
ikbaenjino <enjino@politeknikpgribanten.ac.id>
2025-06-12 22:15:51 +0700

    Create page.tsx

---
+++ app/app/beranda/page.tsx
@@ -0,0 +1,29 @@
+import Hero from "@/components/Hero";
+import Programs from "@/components/Programs";
+import Methods from "@/components/Methods";
+import Stats from "@/components/Stats";
+import dynamic from "next/dynamic";
+import Footer from "@/components/Footer";
+
+const Testimonials = dynamic(() => import("@/components/Testimonials"), { ssr: false });
+const Events = dynamic(() => import("@/components/Events"), { ssr: false });
+const FAQ = dynamic(() => import("@/components/FAQ"), { ssr: false });
+const Locations = dynamic(() => import("@/components/Locations"), { ssr: false });
+
+export default function BerandaPage() {
+  return (
+    <main>
+      <Hero />
+      <Programs />
+      <Methods />
+      <Stats />
+      <Testimonials />
+      <Events />
+      <FAQ />
+      <Locations />
+
+      {/* Footer */}
+      <Footer />
+    </main>
+  );
+}
